0c1d6043ff63d9078082f4cb2521838a2227741b
Yoshihiro Fujita <yoshihiro.fujita@i-Vinci.co.jp>
2022-12-13 06:22:50 +0900

    [modify] 微修正

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -16,7 +16,6 @@ const stack = new CdkDemoStack(app, 'CdkDemoStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'ap-northeast-1' },
-  env: { account: '199376756644', region: 'ap-northeast-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
